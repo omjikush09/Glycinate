2994ed38d8bb5f0ae691992370df637fe2f466a4
Omji Kushwaha <omjimaurya09@gmail.com>
2026-03-12 22:10:05 +0530

    Feat:change the ecr to public

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -4,8 +4,19 @@ set -aexuo pipefail
 source .env
 set +a
 
+#!/bin/bash
 
-npm i -g pnpm@10.10.0
+if ! command -v pnpm &> /dev/null
+then
+    echo "pnpm not found. Installing via corepack..."
+
+    corepack enable
+    corepack prepare pnpm@latest --activate
+
+    echo "pnpm installed successfully."
+else
+    echo "pnpm already installed."
+fi
 cd ../
 pnpm i -g turbo@2.4.4
 turbo run build --filter=@repo/db
@@ -28,13 +39,13 @@ terraform init
 terraform plan 
 terraform apply -target=module.ecr -auto-approve
 
-aws ecr get-login-password --region $AWS_REGION  | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+aws ecr-public get-login-password --region $AWS_REGION  | docker login --username AWS --password-stdin public.ecr.aws/d3r6v0t5/glycinate
 
 cd ../ecs
 
-docker build --platform=linux/arm64 -t  $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/glycinate:latest .
+docker build --platform=linux/arm64 -t  public.ecr.aws/d3r6v0t5/glycinate:latest .
 
-docker push  $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/glycinate:latest
+docker push  public.ecr.aws/d3r6v0t5/glycinate:lastest
 
 # Build Lambda Functions
 
